5c80406907f068d47d20459b4a8a47b36161376f
Ebenezer_Arhin_Forson <ebenforson2002@gmail.com>
2025-09-11 09:20:03 +0000

    Add basic_operations.py file for Task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Assign the given values to the variables
+number1 = 10
+number2 = 5
+
+# Perform the arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results in the specified format
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
